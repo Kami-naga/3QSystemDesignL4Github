9a3dade5dd70030d78002d0c17da855f26fc8835
LAPTOP\hyq <hyq961206@sjtu.edu.cn>
2020-11-27 07:44:11 +0900

    set my repo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,5 @@ if [ $? -ne 0 ]; then
   echo "nothing to commit"
   exit 0
 fi
-git remote set-url origin "git@github.com:uchan-nos/myproj.git"
+git remote set-url origin "git@github.com:Kami-naga/3QSystemDesignL4Github.git"
 git push -f origin $target_branch
